7effbb76d8b224417e21056a5f7f7a6025660c97
RodriAllende <90284758+RodriAllende@users.noreply.github.com>
2023-08-23 15:41:13 -0300

    Second Update
    I did 3 Function trying  to get the computer choices

--- main.js
+++ main.js
@@ -2,14 +2,41 @@
 
 console.log("Aprendiendo");
 
-
-
-
-
 const choices = ["Rock", "Paper", "Scissors"];
 function  computerchoice() {
 const choice =choices[Math.floor(Math.random()*choices.length)];
-console.log(choice);
+return choice;
 }
 
-computerchoice();
+
+function checkingWinner (playerchoice , computerchoice) {
+    if (playerchoice == computerchoice){
+        return "Tie";
+    }
+       else if (
+                (playerchoice == "Rock" && computerchoice =="Scissors") ||
+                (playerchoice == "Scissors" && computerchoice =="Paper") ||
+                (playerchoice == "Paper" && computerchoice =="Rock") 
+       ){
+       return "Player" ;
+    } else {
+    return "Computer" ;
+ 
+    }}
+
+    function playRound (playerchoice,computerchoice) {
+        const result=checkingWinner(playerchoice, computerchoice);
+        if (result =="Tie"){
+            return "Its a Tie" 
+        } else if (result == "Player"){
+            return `You Win ${playerchoice} beat ${computerchoice}`} 
+            else {
+                return `You lose ${computerchoice} beat ${playerchoice}`
+
+            }
+    }
+
+
+    const playerchoice = "Rock";
+    const computerselection =   computerchoice();
+    console.log(playRound(playerchoice, computerselection));
