0106ef84000b794076476d0138871720c8e3f557
RodriAllende <90284758+RodriAllende@users.noreply.github.com>
2023-08-23 12:49:30 -0300

    Very First problema
    I started by dividing the tasks into small blocks to be able to solve everything in the clearest way

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+console.log("Aprendiendo");
+
+
+
+
+
+const choices = ["Rock", "Paper", "Scissors"];
+function  computerchoice() {
+const choice =choices[Math.floor(Math.random()*choices.length)];
+console.log(choice);
+}
+
+computerchoice();
