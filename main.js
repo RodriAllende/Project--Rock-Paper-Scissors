921eaddeeec52f3bfd0ee402439365ba2b84146d
RodriAllende <90284758+RodriAllende@users.noreply.github.com>
2023-10-12 18:32:29 -0300

    ALmost Done

--- main.js
+++ main.js
@@ -2,95 +2,109 @@ console.log("Aprendiendo");
 
 let choices = ["Rock", "Paper", "Scissors"];
 
-function computerchoice() {
+// Variables para llevar un registro de las puntuaciones
+let playerScore = 0;
+let computerScore = 0;
+
+// Función para actualizar el marcador en el HTML
+function updateScoreboard() {
+  const playerScoreElement = document.getElementById('Player1');
+  const computerScoreElement = document.getElementById('Computer');
+
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+}
+
+function computerChoice() {
   const choice = choices[Math.floor(Math.random() * choices.length)];
   return choice;
 }
 
-function checkingWinner(playerchoice, computerchoice) {
-  const playerWins = 
-    (playerchoice == "Rock" && computerchoice == "Scissors") ||
-    (playerchoice == "Scissors" && computerchoice == "Paper") ||
-    (playerchoice == "Paper" && computerchoice == "Rock");
-
-  if (playerchoice == computerchoice) {
+function checkingWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
     return "Tie";
-  } else if (playerWins) {
+  } else if (
+    (playerChoice === "Rock" && computerChoice === "Scissors") ||
+    (playerChoice === "Scissors" && computerChoice === "Paper") ||
+    (playerChoice === "Paper" && computerChoice === "Rock")
+  ) {
     return "Player";
   } else {
     return "Computer";
   }
 }
 
-function playRound(playerchoice, computerchoice) {
-  const result = checkingWinner(playerchoice, computerchoice);
-  if (result == "Tie") {
+function playRound(playerChoice, computerChoice) {
+  const result = checkingWinner(playerChoice, computerChoice);
+  if (result === "Tie") {
     return "It's a Tie!";
-  } else if (result == "Player") {
-    return `You Win! ${playerchoice} beat ${computerchoice}`;
+  } else if (result === "Player") {
+    playerScore++; // Incrementar la puntuación del jugador
+    return `You Win! ${playerChoice} beats ${computerChoice}`;
   } else {
-    return `You lose! ${computerchoice} beat ${playerchoice}`;
+    computerScore++; // Incrementar la puntuación de la computadora
+    return `You lose! ${computerChoice} beats ${playerChoice}`;
   }
 }
 
+// Función para mostrar un resultado en el tablero de puntuación
 function displayResult(result) {
-  const scoreBoard = document.getElementById('score-board');
-  const resultElement = document.createElement('p');
-  resultElement.textContent = result;
-  scoreBoard.appendChild(resultElement);
+  const roundWinnerElement = document.getElementById('round-winner');
+  roundWinnerElement.textContent = result;
+
+  // Actualizar el marcador después de cada ronda
+  updateScoreboard();
+
+  // Verificar si un jugador ha ganado 5 veces para finalizar el juego
+  if (playerScore === 5 || computerScore === 5) {
+    endGame();
+  }
 }
 
-function handlePlayerChoice(playerChoice) {
-  console.log(`Jugador eligió: ${playerChoice}`);
-  const computerSelection = computerchoice();
-  const result = playRound(playerChoice, computerSelection);
-  displayResult(result);
+// Función que muestra al ganador del juego
+function endGame() {
+  const winnerMessage = document.getElementById('winner-message');
+  if (playerScore === 5) {
+    winnerMessage.textContent = "¡El jugador ha ganado el juego!";
+  } else if (computerScore === 5) {
+    winnerMessage.textContent = "¡La computadora ha ganado el juego!";
+  }
+}
+
+// Función para reiniciar el juego
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  updateScoreboard();
+  document.getElementById('round-winner').textContent = "";
+  document.getElementById('winner-message').textContent = "";
 }
 
 // Asociar eventos de clic a los botones de elección del jugador
-document.getElementById('rock-button').addEventListener('click', function() {
+document.getElementById('rock-button').addEventListener('click', function () {
   handlePlayerChoice('Rock');
 });
 
-document.getElementById('paper-button').addEventListener('click', function() {
+document.getElementById('paper-button').addEventListener('click', function () {
   handlePlayerChoice('Paper');
 });
 
-document.getElementById('scissors-button').addEventListener('click', function() {
+document.getElementById('scissors-button').addEventListener('click', function () {
   handlePlayerChoice('Scissors');
 });
+
+function handlePlayerChoice(playerChoice) {
+  if (playerScore < 5 && computerScore < 5) {
+    console.log(`Jugador eligió: ${playerChoice}`);
+    const computerSelection = computerChoice();
+    const result = playRound(playerChoice, computerSelection);
+    displayResult(result);
+  }
+}
 
 // Función que maneja el juego completo
 function game() {
   console.log("Welcome!");
-
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    // Simular elección del jugador a través del evento de clic
-    // No es necesario obtener la elección del jugador aquí
-    // Las elecciones se manejarán a través de los eventos de clic
-    const computerselection = computerchoice();
-
-    console.log(playRound(computerselection, "Rock")); // Cambia esto para reflejar la elección real del jugador
-
-    const winner = checkingWinner("Rock", computerselection); // Cambia "Rock" por la elección real del jugador
-    if (winner == "Player") {
-      playerScore++;
-    } else if (winner == "Computer") {
-      computerScore++;
-    }
-  }
-
-  console.log("Game over!");
-
-  if (playerScore > computerScore) {
-    console.log("Player was the winner!");
-  } else if (playerScore < computerScore) {
-    console.log("Computer was the winner!");
-  } else {
-    console.log("We have a tie!");
-  }
 }
 
 // Iniciar el juego
